ef241c61598266af90c07a3a2e933c4288689631
mocharl3 <mosescharles910@gmail.com>
2022-12-15 03:20:23 -0800

    functions in nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	char uppercase = 'A';
+
+	int _isupper = 0;
+
+
+
+	for (; uppercase <= 'Z'; uppercase++)
+
+	{
+
+		if (c == uppercase)
+
+		{
+
+			isupper = 1;
+
+			break;
+
+		}
+
+	}
+
+
+
+	return (isupper);
+
+}
